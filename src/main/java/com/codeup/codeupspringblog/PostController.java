1074e9a6f55adf1b4e7b25ce3682c7ed714b87ff
William Clayton <william.d.clayton1@gmail.com>
2023-11-20 09:17:44 -0600

    -wc

--- src/main/java/com/codeup/codeupspringblog/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/PostController.java
@@ -4,13 +4,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 @Controller
 public class PostController {
 
     @RequestMapping(path = "/post", method = RequestMethod.GET)
-    @ResponseBody
-    public String post() {
-        return "posts index page";
+    public String post(Model model) {
+
+        ArrayList<Post> postList = new ArrayList<>();
+
+        Post post1 = new Post();
+        Post post2 = new Post();
+
+        post1.setTitle("Title1");
+        post1.setBody("Body1");
+
+        post2.setTitle("Title2");
+        post2.setBody("Body2");
+
+        postList.add(post1);
+        postList.add(post2);
+
+        model.addAttribute("postList", postList);
+        return "posts/index";
+
+
     }
 
     @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
